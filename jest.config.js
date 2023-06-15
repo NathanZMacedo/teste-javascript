195e49061aeb273b4fef9bd1fe303b4285f41bbb
nathanzmacedo <johnsupergeeks@gmail.com>
2023-06-15 17:32:35 -0300

    teste finalizado

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     clearMocks: true,
     setupFilesAfterEnv: ['regenerator-runtime/runtime'],
-    testPathIgnorPatterns: [
+    testPathIgnorePatterns: [
         "/node_modules/",
     ]
 }
---
+++ src/elementos.test.js
@@ -0,0 +1,26 @@
+import { getByText} from '@testing-library/dom';
+import '@testing-library/jest-dom/extend-expect'
+import {JSDOM} from 'jsdom'
+import fs from 'fs'
+import path from 'path'
+
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+let dom 
+let container
+
+describe('testando o index.html', ()=>{
+    beforeEach (()=>{
+        const options= {
+            resources: 'usable',
+            runScripts: 'dangerously',
+        }
+        dom = new JSDOM(html,options)
+        container = dom.window.document.body
+    })
+    it('checando se eu tenho um titulo h1',()=>{
+        let h1Element = container.querySelector('h1')
+        expect(h1Element).not.toBeNull()
+        let textH1 = getByText(container,"Testando Apps")
+        expect(textH1).toBeInTheDocument()
+    })
+})
